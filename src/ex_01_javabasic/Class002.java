dcf6294e61f54a72780800a6c837c222bdff0dd1
Ashfaq hussain <ashfaq.mah1310@gmail.com>
2025-12-10 23:06:10 +0530

    Class002

---
+++ src/ex_01_javabasic/Class002.java
@@ -0,0 +1,11 @@
+package ex_01_javabasic;
+
+import java.sql.SQLOutput;
+
+public class Class002 {
+    public static void main(String[] args) {
+        System.out.println("Working session");
+        System.out.println("Practice session");
+
+    }
+}
